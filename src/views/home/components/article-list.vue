88624f1f7f7c245afb2776f0631cc63e90d9612f
Full Version <fullversion@FulldeMacBook-Air.local>
2022-05-02 13:01:20 +0800

    记住滚动条的位置

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="article-list">
     <van-pull-refresh
       v-model="isreFreshLoading"
       @refresh="onRefresh"
@@ -101,4 +101,11 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.article-list {
+  // 百分比单位相对于父元素
+  // height: 100%;
+  height: 79vh;
+  overflow-y: auto;
+}
+</style>
